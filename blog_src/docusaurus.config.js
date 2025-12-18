832829ec3e87eb5784fb2de66a0f2e949be1c9af
markosioannou <145615754+markosioannou@users.noreply.github.com>
2025-12-18 15:34:46 +0200

    test

--- blog_src/docusaurus.config.js
+++ blog_src/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://markos-ioannou.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/blog/',
+  baseUrl: '/blog/', 
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
